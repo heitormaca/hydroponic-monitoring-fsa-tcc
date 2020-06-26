db45b53c300fa4a13389c48827d0affbc6736abd
Jonathan Bertoldi <jonathanbertoldi7@gmail.com>
2020-06-25 22:27:35 -0300

    Adding healthcheck controller

---
+++ api/Hydroponics/Hydroponics.Api/Controllers/HealthCheckController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hydroponics.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class HealthCheckController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(new { ServiceStatus = "Up", Moment = DateTime.Now });
+        }
+    }
+}
